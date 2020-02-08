92fcb18cf9947d81397bfc3f968154e5a6ba19bc
zhijunhong <981827802@qq.com>
2020-02-08 14:54:06 +0800

    1.更新build.gradle 2.验证jdk注解

--- app/src/main/java/fudaojun/com/custom_view/MainActivity.java
+++ app/src/main/java/fudaojun/com/custom_view/MainActivity.java
@@ -1,7 +1,10 @@
 package fudaojun.com.custom_view;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresPermission;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //添加权限注解
+        openFile();
+    }
+
+    @RequiresPermission(allOf = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE})
+    private void openFile() {
+
     }
 }
